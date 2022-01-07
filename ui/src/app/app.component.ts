b8a0219a94317280d56d29bf26b02179212980e5
Kenny Musterer <kennymu33@gmail.com>
2022-01-07 14:34:23 +0100

    First UI-developments

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare let Neutralino: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'liteman-ng';
+
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+
+  }
 }
--- ui/src/app/components/titlebar/titlebar.component.ts
+++ ui/src/app/components/titlebar/titlebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 declare let Neutralino: any;
 
@@ -11,17 +11,46 @@ export class TitlebarComponent implements OnInit {
 
   constructor() { }
 
+  dragging = false;
+  xOffset = 0;
+
   ngOnInit(): void {
-    Neutralino.window.setDraggableRegion('title');
+  }
+
+  @HostListener('document:mousemove', ['$event']) 
+  onMouseMove(e) {
+    if (this.dragging) Neutralino.window.move(e.screenX*2  - this.xOffset, e.screenY*2);
+  }
+
+  @HostListener('window:mouseup', ['$event'])
+  mouseUp(event){
+    this.dragging = false;
+
+    if(event.screenY == 0){
+      Neutralino.window.maximize();
+    }
+  }
+
+  async grab(e) {
+    this.xOffset = e.screenX - e.pageX;
+    this.dragging = true;
+
+    if (await Neutralino.window.isMaximized()) {
+      Neutralino.window.unmaximize();
+    }
   }
 
   minimize() {
     Neutralino.window.minimize();
   }
 
-  maximize() {
-    if (Neutralino.window.isMaximized()) {
+  async switchSize() {
+    
+    console.log(await Neutralino.window.isMaximized());
+    
+    if (await Neutralino.window.isMaximized()) {
       Neutralino.window.unmaximize();
+      if(this.dragging) this.dragging = false;
     }
     else {
       Neutralino.window.maximize();
@@ -32,3 +61,7 @@ export class TitlebarComponent implements OnInit {
     Neutralino.app.exit();
   }
 }
+function allRelationshipQueries(allRelationshipQueries: any) {
+  throw new Error('Function not implemented.');
+}
+
