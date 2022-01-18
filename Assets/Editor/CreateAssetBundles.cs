410ee89fe2bd9e6c23156d3e3c8ab160e0bf7b63
Kenny Musterer <kennymu33@gmail.com>
2022-01-18 14:31:32 +0100

    Added namespace to editor-script

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -2,30 +2,32 @@
 using UnityEngine;
 using System.IO;
 
-public class CreateAssetBundles
+namespace ABG
 {
-    [MenuItem("Assets/Build AssetBundles")]
-    static void BuildAllAssetBundles()
+    public class CreateAssetBundles
     {
-        Object[] objectsToConvert = Resources.LoadAll("Conversion");
-        Debug.Log(objectsToConvert);
-        foreach (Object objectToConvert in objectsToConvert)
+        [MenuItem("Assets/Build AssetBundles")]
+        static void BuildAllAssetBundles()
         {
-            string assetPath = AssetDatabase.GetAssetPath(objectToConvert);
-            string objectName = Path.GetFileNameWithoutExtension(assetPath);
-            Debug.Log(objectName);
-            AssetImporter.GetAtPath(assetPath).SetAssetBundleNameAndVariant(objectName, "");
-        }
+            Object[] objectsToConvert = Resources.LoadAll("Conversion");
+            Debug.Log(objectsToConvert);
+            foreach (Object objectToConvert in objectsToConvert)
+            {
+                string assetPath = AssetDatabase.GetAssetPath(objectToConvert);
+                string objectName = Path.GetFileNameWithoutExtension(assetPath);
+                AssetImporter.GetAtPath(assetPath).SetAssetBundleNameAndVariant(objectName, "");
+            }
 
-        string assetBundleDirectory = Path.Combine("Assets/Resources/Converted");
-        if (!Directory.Exists(assetBundleDirectory))
-        {
-            Directory.CreateDirectory(assetBundleDirectory);
-        }
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
-                                        BuildAssetBundleOptions.None,
-                                        BuildTarget.StandaloneWindows64);
+            string assetBundleDirectory = Path.Combine("Assets/Resources/Converted");
+            if (!Directory.Exists(assetBundleDirectory))
+            {
+                Directory.CreateDirectory(assetBundleDirectory);
+            }
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+                                            BuildAssetBundleOptions.None,
+                                            BuildTarget.StandaloneWindows64);
 
-        AssetDatabase.Refresh();
+            AssetDatabase.Refresh();
+        }
     }
 }
